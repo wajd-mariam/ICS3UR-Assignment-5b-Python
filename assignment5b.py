cd707f5d04b0c7b8ac9d6dfb1915f02761f00687
Ubuntu <ubuntu@ip-172-31-27-134.ec2.internal>
2019-11-11 02:19:21 +0000

    Program add

---
+++ assignment5b.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Wajd Mariam
+# Created on: November 2019
+# This program calculates the multiplication table 0-10 of the number entered
+
+
+def main():
+    # counter & answer
+    counter = 0
+    answer = 0
+
+    # input
+    number = int(input("Enter number to show it's multiplication table:"))
+
+    # try & process & output
+    try:
+        while counter < 11:
+            answer = (counter * number)
+            print("{} x {} = {}".format(counter, number, answer))
+            counter = counter + 1
+    except Exception:
+        print("Invalid Input")
+    finally:
+        print("Thank you for using my program!")
+
+
+if __name__ == "__main__":
+    main()
